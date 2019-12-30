7fa0bf3da7ba592a0666e340d66f2210ced3c83a
Alyona <alyona.rodin@gmail.com>
2019-12-29 19:48:59 -0500

    first commit

--- GameofLife.js
+++ GameofLife.js
@@ -4,39 +4,29 @@ class GameOfLife {
     this.height = height;
     this.board = this.makeBoard();
   }
-
-  /**
-   * Returns a 2D Array
-   */
-
   makeBoard() {
     let arr = []
 
     for (let y = 0; y < this.height; y++) {
-      let arr_inner = []
+      let arrInner = []
       for (let x = 0; x < this.width; x++) {
-        arr_inner.push(0)
+        arrInner.push(0)
       }
-      arr.push(arr_inner)
+      arr.push(arrInner)
     }
     return arr
   }
-
-
-  /**
-   * Return the amount of living neighbors around a given coordinate.
-   */
   get(row, column) {
     return this.board[row][column]
   }
   livingNeighbors(row, col) {
     const reducer = (acc, currV) => acc + currV;
-    const possible_n = [[row - 1, col - 1], [row - 1, col], [row - 1, col + 1],
+    const possibleN = [[row - 1, col - 1], [row - 1, col], [row - 1, col + 1],
     [row, col - 1], [row, col + 1],
     [row + 1, col - 1], [row + 1, col], [row + 1, col + 1]]
     const width = this.width
     const height = this.height
-    return possible_n.filter(function (e) {
+    return possibleN.filter(function (e) {
       if (e[0] < 0 || e[1] < 0 || e[0] >= height || e[1] >= width) {
         return false
       }
@@ -48,11 +38,6 @@ class GameOfLife {
     }.bind(this)).reduce(reducer, 0)
 
   }
-
-
-  /**
-   * Given the present board, apply the rules to generate a new board
-   */
   randomize(){
     const newBoard = this.makeBoard();
     for (let row=0; row<this.height; row++){
@@ -65,22 +50,11 @@ class GameOfLife {
   }
   tick() {
     const newBoard = this.makeBoard();
-    // TODO: Here is where you want to loop through all the cells
-    // on the existing board and determine, based on it's neighbors,
-    // whether the cell should be dead or alive in the new board
-    // (the next iteration of the game)
-    //
-    // You need to:
-    // 1. Count alive neighbors for all cells
-    // 2. Set the next state of all cells in newBoard,
-    // based on their current alive neighbors
     for (let row = 0; row < this.height; row++) {
       for (let col = 0; col < this.width; col++) {
         const cell = this.get(row, col)
         const numNeighbors = this.livingNeighbors(row, col);
-        // copy over last state to begin with
         newBoard[row][col] = cell;
-        // change board according to rules
         if (numNeighbors < 2) {
           newBoard[row][col] = 0;
         } else if (numNeighbors > 3) {
